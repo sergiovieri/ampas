ebfa12c062c0ef1fc5f775a90e602bc11c0e62d4
lungsin <kweelungsin@gmail.com>
2021-11-06 17:53:46 +0800

    View: Add UserGroupViewSet

--- backend/splitbill/views.py
+++ backend/splitbill/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+
+from splitbill.models.usergroup import UserGroup
+from splitbill.serializers.usergroup import UserGroupSerializer
 
 # Create your views here.
+
+
+class UserGroupViewSet(viewsets.ModelViewSet):
+    """
+    List all user groups, or create a new group.
+    """
+
+    queryset = UserGroup.objects.all()
+    serializer_class = UserGroupSerializer
